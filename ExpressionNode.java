1098f7816dda008ff1ca1f8b8e7c1f645e0137d4
Winnie Ly <wsly@wpi.edu>
2018-12-05 17:10:54 -0500

    Add converToString to ExpressionNode and JavaDoc

--- ExpressionNode.java
+++ ExpressionNode.java
@@ -33,9 +33,45 @@ public abstract class ExpressionNode implements Expression
 		{
 			
 		}
-		
-		public void convertToString(StringBuilder stringBuilder, int indentLevel)
-		{
-			
+
+
+	/**
+	 * Method that recursively calls itself to create a String of type Expression and its children
+	 * that starts at the given indent level.
+	 * @param stringBuilder the stringBuilder that appends a name and a new line to create a String
+	 * @param indentLevel the current indentation level
+	 */
+
+	public void convertToString(StringBuilder stringBuilder, int indentLevel)
+	{
+		indent(stringBuilder, indentLevel);
+
+		stringBuilder.append(_data);
+
+		stringBuilder.append('\n');
+
+		for (Expression expr : _children) {
+
+			expr.convertToString(stringBuilder, indentLevel + 1);
+
 		}
+
+	}
+
+	/**
+	 * Helper method for indentation for StirngBuilder
+	 * @param stringBuilder the stringBuilder that appends each tab character
+	 * @param indentLevel the number of tabs that must be appended to StirngBuilder
+	 */
+
+
+	public static void indent(StringBuilder stringBuilder, int indentLevel) {
+
+		for(int i = 0; i < indentLevel; i++) {
+
+			stringBuilder.append('\t');
+
+		}
+
+	}
 }
