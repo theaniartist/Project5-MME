3b12d2c13248b4fdde7e455e6dcfa67bdb8b4d9c
MatthewAguiar <mattaguiar199@gmail.com>
2018-12-06 19:25:33 -0500

    Almost done with parser

--- Main.java
+++ Main.java
@@ -6,7 +6,9 @@ public class Main
 	public static void main(String[] args) 
 	{
 		SimpleExpressionParser parser = new SimpleExpressionParser();
-		CompoundExpressionNode expression = (CompoundExpressionNode) parser.parseExpression("((9))*x+8+(9*80+9)");
+		//CompoundExpressionNode expression = (CompoundExpressionNode) parser.parseExpression("((9))*x+8+(9*80+9)");
+		CompoundExpressionNode expression = (CompoundExpressionNode) parser.parseExpression("1+1*(2+1)");
+		//System.out.println(((ExpressionNode)expression.getSubexpressions().get(0))._data);
 		expression.convertToString(new StringBuilder(), 0);
 		ExpressionNode node = (((ExpressionNode) expression.getSubexpressions().get(1)));
 		//System.out.println(node._data);
